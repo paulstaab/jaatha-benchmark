c3a28488ffbbef186b1c1223c6b322940240245d
Paul R. Staab <staab@bio.lmu.de>
2013-11-19 10:14:50 +0100

    update script

--- test_jaatha.R
+++ test_jaatha.R
@@ -22,34 +22,34 @@ runTest <- function(dm, n.points=2, seed=12523, model){
   n <- dim(par.grid)[1]
   seeds <- sample.int(2^20, n)
 
-  folder <- paste("results", "/", version, "/", model, "/", sep="")
-  log.folder <- paste("logs", "/", version, "/", model, "/", sep="")
+  folder <- paste("results", version, model, sep="/")
+  log.folder <- paste("logs", version, model, sep="/")
   dir.create(folder, recursive=T, showWarnings=F)
   dir.create(log.folder, recursive=T, showWarnings=F)
 
-  registerDoMC(32)
+  registerDoMC(16)
 
   results <- foreach(i=1:n, .combine=rbind) %dopar% { 
       cat("Run",i,"of",n,"\n")
-      sink(file=paste(log.folder, "run_", i, ".txt", sep=""))
+      sink(file=paste(log.folder, "/run_", i, ".txt", sep=""))
       set.seed(seeds[i])
       cat("----------------------------------------------------------------------\n")
       cat("Run",i,"of",n,"\n")
       cat("Real parameters:",par.grid[i,],"\n")
       cat("----------------------------------------------------------------------\n")
       jsfs <- dm.simSumStats(dm, par.grid[i, ])
-      jaatha <- Jaatha.initialize(dm, jsfs=jsfs, use.shm=TRUE, cores=1)
+      jaatha <- Jaatha.initialize(dm, jsfs=jsfs, use.shm=TRUE, cores=2)
 
       runtimes <- rep(0, 6)
       names(runtimes) <-
         c('init.user','init.system','init.elapsed','ref.user','ref.system','ref.elapsed')
 
       runtimes[1:3] <- system.time(
-        jaatha <- Jaatha.initialSearch(jaatha, 100, 3)
+        jaatha <- Jaatha.initialSearch(jaatha, 100, 2)
       )
 
       runtimes[4:6] <- system.time(
-        jaatha <- Jaatha.refinedSearch(jaatha, 5 , 100, epsilon=2, sim.final=100)
+        jaatha <- Jaatha.refinedSearch(jaatha, 2 , 100, 100, epsilon=1)
       )
       estimates <-  Jaatha.getLikelihoods(jaatha)[1,-(1:2)]
       sink(NULL)
@@ -59,9 +59,9 @@ runTest <- function(dm, n.points=2, seed=12523, model){
   estimates <- results[, -(1:6)]
   runtimes <- results[, 1:6]
 
-  write.table(estimates, file=paste(folder, "estimates.txt", sep=""), row.names=F)
-  write.table(par.grid,  file=paste(folder, "true_values.txt", sep=""), row.names=F)
-  write.table(runtimes,  file=paste(folder, "runtimes.txt", sep=""), row.names=F)
+  write.table(estimates, file=paste(folder, "estimates.txt", sep="/"), row.names=F)
+  write.table(par.grid,  file=paste(folder, "true_values.txt", sep="/"), row.names=F)
+  write.table(runtimes,  file=paste(folder, "runtimes.txt", sep="/"), row.names=F)
 }
 
 evalTest <- function(test.results){
@@ -137,4 +137,4 @@ dm.mg <- dm.addMigration(dm.mg, .1, 5, pop.from=2, pop.to=1,
                          new.par.name="m21")
 dm.mg <- dm.addGrowth(dm.mg, .1, 5, population=2)
 dm.mg <- dm.addRecombination(dm.mg, fixed=20)
-runTest(dm.mg, 2, model="mg")
+#runTest(dm.mg, 2, model="mg", seed=1238714924)
