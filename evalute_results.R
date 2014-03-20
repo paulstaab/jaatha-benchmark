08fd80be46d3470eb8442b1a09e6bfa63632c24a
Paul R. Staab <staab@bio.lmu.de>
2014-03-20 10:17:35 +0100

    update scripts

--- evalute_results.R
+++ evalute_results.R
@@ -22,14 +22,13 @@ res.model   <- c()
 res.mse     <- c()
 res.run.time <- c()
 
-versions <- list.dirs("results", recursive=F)
+versions <- list.dirs("runs", recursive=F)
 for (version in versions) {
-  version.name <- list.dirs()
   models <- list.dirs(version, recursive=F)
   for (model in models) {
-    file.est <- paste(model, "estimates.txt", sep="/")
-    file.true.values <- paste(model, "/true_values.txt", sep="")
-    file.run.times <- paste(model, "/runtimes.txt", sep="")
+    file.est <- paste(model, 'results', "estimates.txt", sep="/")
+    file.true.values <- paste(model, 'results', "true_values.txt", sep="/")
+    file.run.times <- paste(model, 'results', "runtimes.txt", sep="/")
 
     if (!(file.exists(file.est) & file.exists(file.true.values) &
           file.exists(file.run.times)) ) {
@@ -41,7 +40,8 @@ for (version in versions) {
     true.values <- read.table(file.true.values, header=T)
     run.times <- read.table(file.run.times, header=T)
 
-    mse <- mean((estimates - true.values)^2)
+    #mse <- mean((estimates - true.values)^2)
+    mse <- mean(apply(abs(log10(estimates/true.values)), 2, mean))
     avg.run.time <- mean(run.times[, 3] + run.times[,6])
     version <- strsplit(model, "/")[[1]][2]
     model <- strsplit(model, "/")[[1]][3]
@@ -58,4 +58,4 @@ result <- data.frame(Version=res.version,
            MSE=res.mse,
            Run.Time=res.run.time )
 
-result
+print(result)
--- test_jaatha.R
+++ test_jaatha.R
@@ -1,25 +1,46 @@
 #!/usr/bin/Rscript --vanilla
 
-# Tests the current installed version of Jaatha using
-# a simple theta/tau model
+arg <- commandArgs()
+path <- '/scratch/paul/test_jaatha/lib'
 
-library(jaatha)
+if (!is.na(arg[2])) {
+  stopifnot(file.exists(arg[2]))
+  jaatha.package <- arg[2]
+} else {
+  jaatha.package <- 'packages/jaatha_2.97.2.tar.gz'
+}
+
+dir.create(path, recursive=TRUE)
+install.packages(jaatha.package, dependencies=TRUE, lib=path) 
+library(jaatha, lib.loc=path)
 library(testJaatha)
 
 version <- as.character(packageVersion("jaatha"))
 
-#Test a simple theta/tau/migration model
+# Test a simple theta/tau/migration model
 dm <- dm.createDemographicModel(c(20,25), 75)
 dm <- dm.addSpeciationEvent(dm, 0.01, 5)
 dm <- dm.addMutation(dm, 1, 10)
 dm <- dm.addRecombination(dm, fixed=5)
 dm <- dm.addSymmetricMigration(dm, .1, 2)
-testJaatha:::testJaatha(dm, 3, 2, seed=12579, smoothing=FALSE, cores=c(32,1),
+testJaatha:::testJaatha(dm, 3, 2, seed=12579, smoothing=FALSE, cores=c(8,4),
                         folder=paste('runs', version, 'tt.classic', sep='/'))
-testJaatha:::testJaatha(dm, 3, 2, seed=12579, smoothing=TRUE, cores=c(32,1),
+testJaatha:::testJaatha(dm, 3, 2, seed=12579, smoothing=TRUE, cores=c(8,4),
                         folder=paste('runs', version, 'tt.smooth', sep='/'))
 
-#Test a model with 5 parameters
+
+# Test the fpc sum.stat
+dm.fpc <- dm.createDemographicModel(c(20,25), 100)
+dm.fpc <- dm.addSpeciationEvent(dm.fpc, 0.01, 5)
+dm.fpc <- dm.addMutation(dm.fpc, 1, 10)
+dm.fpc <- dm.addRecombination(dm.fpc, 1, 10)
+dm.fpc <- dm.addSymmetricMigration(dm.fpc, .1, 2)
+testJaatha:::testJaatha(dm.fpc, 2, 2, seed=124578, smoothing=TRUE, cores=c(8, 4),
+                        folder=paste('runs', version, 'fpc', sep='/'),
+                        fpc=TRUE)
+
+
+# Test a model with 5 parameters
 dm.mg <- dm.createDemographicModel(c(20,25), 100)
 dm.mg <- dm.addSpeciationEvent(dm.mg,.001,5)
 dm.mg <- dm.addMutation(dm.mg,1,20)
