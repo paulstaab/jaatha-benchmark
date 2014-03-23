17cdfd590e996c8b34a13bd4d56dba886c354ba7
Paul R. Staab <staab@bio.lmu.de>
2014-03-23 10:11:42 +0100

    add results of 2.2 and 2.3 for tt model

--- test_jaatha.R
+++ test_jaatha.R
@@ -7,7 +7,7 @@ if (!is.na(arg[2])) {
   stopifnot(file.exists(arg[2]))
   jaatha.package <- arg[2]
 } else {
-  jaatha.package <- 'packages/jaatha_2.97.2.tar.gz'
+  jaatha.package <- 'packages/jaatha_2.3.1.1.tar.gz'
 }
 
 dir.create(path, recursive=TRUE)
@@ -23,9 +23,9 @@ dm <- dm.addSpeciationEvent(dm, 0.01, 5)
 dm <- dm.addMutation(dm, 1, 10)
 dm <- dm.addRecombination(dm, fixed=5)
 dm <- dm.addSymmetricMigration(dm, .1, 2)
-testJaatha:::testJaatha(dm, 3, 2, seed=12579, smoothing=FALSE, cores=c(8,4),
+testJaatha:::testJaatha(dm, 3, 2, seed=12579, smoothing=FALSE, cores=c(16,2),
                         folder=paste('runs', version, 'tt.classic', sep='/'))
-testJaatha:::testJaatha(dm, 3, 2, seed=12579, smoothing=TRUE, cores=c(8,4),
+testJaatha:::testJaatha(dm, 3, 2, seed=12579, smoothing=TRUE, cores=c(16,2),
                         folder=paste('runs', version, 'tt.smooth', sep='/'))
 
 
@@ -35,22 +35,24 @@ dm.fpc <- dm.addSpeciationEvent(dm.fpc, 0.01, 5)
 dm.fpc <- dm.addMutation(dm.fpc, 1, 10)
 dm.fpc <- dm.addRecombination(dm.fpc, 1, 10)
 dm.fpc <- dm.addSymmetricMigration(dm.fpc, .1, 2)
-testJaatha:::testJaatha(dm.fpc, 2, 2, seed=124578, smoothing=TRUE, cores=c(8, 4),
-                        folder=paste('runs', version, 'fpc', sep='/'),
-                        fpc=TRUE)
+# testJaatha:::testJaatha(dm.fpc, 2, 2, seed=124578, smoothing=TRUE, cores=c(8, 4),
+#                         folder=paste('runs', version, 'fpc', sep='/'),
+#                         fpc=TRUE)
 
 
 # Test a model with 5 parameters
-dm.mg <- dm.createDemographicModel(c(20,25), 100)
-dm.mg <- dm.addSpeciationEvent(dm.mg,.001,5)
-dm.mg <- dm.addMutation(dm.mg,1,20)
-dm.mg <- dm.addMigration(dm.mg, .1, 5, pop.from=1, pop.to=2,
-                         new.par.name="m12")
-dm.mg <- dm.addMigration(dm.mg, .1, 5, pop.from=2, pop.to=1, 
-                         new.par.name="m21")
-dm.mg <- dm.addGrowth(dm.mg, .1, 5, population=2)
-dm.mg <- dm.addRecombination(dm.mg, fixed=20)
-testJaatha:::testJaatha(dm.mg, 2, 2, seed=24680, smoothing=FALSE, cores=(32,1),
-                        folder=paste('runs', version, 'mg.classic', sep='/'))
-testJaatha:::testJaatha(dm.mg, 2, 2, seed=24680, smoothing=TRUE, cores=(32,1),
-                        folder=paste('runs', version, 'mg.smooth', sep='/'))
+dm.gro <- dm.createDemographicModel(c(24,25), 100, 1000)
+dm.gro <- dm.addMutation(dm.gro, 1, 20, new.par.name="theta")
+dm.gro <- dm.addSpeciationEvent(dm.gro, 0.001, 20, new.time.point.name="tau")
+dm.gro <- dm.addSymmetricMigration(dm.gro, 0.005, 5, new.par.name="m")
+dm.gro <- dm.addRecombination(dm.gro, fixed=5)
+dm.gro <- dm.addSizeChange(dm.gro, 0.05, 10, population=2, at.time="0", new.par.name="q")
+dm.gro <- dm.addParameter(dm.gro, par.name="s1", 0.05, 10)
+dm.gro <- dm.addParameter(dm.gro, par.name="s2", 0.05, 10)
+dm.gro <- dm.addSizeChange(dm.gro, par.new=FALSE, parameter="s1+s2", population=1, at.time="tau")
+dm.gro <- dm.addGrowth(dm.gro, par.new=FALSE, parameter="log(1/s1)/tau", population=1)
+dm.gro <- dm.addGrowth(dm.gro, par.new=FALSE, parameter="log(1/s2)/tau", population=2)
+testJaatha:::testJaatha(dm.gro, 2, 1, seed=24680, smoothing=FALSE, cores=c(16, 2),
+                        folder=paste('runs', version, 'gro.old', sep='/'))
+testJaatha:::testJaatha(dm.mg, 2, 1, seed=24680, smoothing=TRUE, cores=c(16, 2),
+                        folder=paste('runs', version, 'gro.sm', sep='/'))
